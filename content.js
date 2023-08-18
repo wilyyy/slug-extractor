61af7ddb60d1d814f000b112319bd2243ab07f48
William Alvarez <walvarez1@my.bcit.ca>
2023-08-17 18:01:27 -0700

    new feature setup, no logic yet

--- content.js
+++ content.js
@@ -1,4 +1,19 @@
-const ExtractSlug = (depth) => {
+/**
+ * This function will copy paste the whole slug
+ */
+const ExtractWholeSLug = () => {};
+
+/**
+ * This function will open a new tab with localhost/slug
+ * @param {string} host how deep in the within the slug we want to copy
+ */
+const RunLocalHostWithSlug = (host) => {};
+
+/**
+ * This function extracts a portion of the slug based on breadcrumb depth value
+ * @param {number} depth how deep in the within the slug we want to copy
+ */
+const ExtractSlugWithDepth = (depth) => {
    try {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
          const url = new URL(tabs[0].url);
@@ -24,13 +39,16 @@ const ExtractSlug = (depth) => {
    }
 };
 
+// onload function
 document.addEventListener("DOMContentLoaded", function () {
-   const extractButton = document.querySelector(".extract-button");
+   const extractWholeSlugButton = document.querySelector(".extract-whole-slug");
+   const runLocalHostButton = document.querySelector(".run-local-host");
+   const extractWithDepthButton = document.querySelector(".extract-slug-with-depth");
 
-   extractButton.addEventListener("click", function () {
+   extractWithDepthButton.addEventListener("click", function () {
       const depthInput = document.querySelector(".depth-input");
       const depth = parseInt(depthInput.value, 10);
 
-      ExtractSlug(depth);
+      ExtractSlugWithDepth(depth);
    });
 });
